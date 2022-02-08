64d463b2297487a3495d9573f26b3597ff4a4eda
Bernard_Addo_boadu <Bernard_Addo_boadu@dell.com>
2022-02-08 17:22:58 -0600

    fixing bulb exception in light service

--- light_service.py
+++ light_service.py
@@ -1,4 +1,5 @@
-from yeelight import Bulb
+from time import sleep
+from yeelight import Bulb, BulbException
 from decouple import config
 from enum import Enum
 
@@ -10,7 +11,11 @@ class LightType(Enum):
 
 class LightService:
     floor_lamp = Bulb(LIVING_ROOM_FLOOR_LAMP)
-    
+    floor_lamp.start_music()
+    # print("came to static field first")
+
+    def __init__(self) -> None:
+        print ("came to constructor")
 
     @staticmethod
     def turn_on(light_type: object):
@@ -22,6 +27,9 @@ class LightService:
                 try:
                     LightService.floor_lamp.turn_on()
                     status = "successful"
+                except BulbException as e:
+                    print (f'Attempt {attempt}: {e}')
+                    status = "error"
                 except Exception as e:
                     print (f'Attempt {attempt}: {e}')
                     status = "error"
@@ -39,6 +47,9 @@ class LightService:
                 try:
                     LightService.floor_lamp.turn_off()
                     status = "succesful"
+                except BulbException as e:
+                    print (f'Attempt {attempt}: {e}')
+                    status = "error"
                 except Exception as e:
                     print (f'Attempt {attempt}: {e}')
                     status = "error"
@@ -52,7 +63,7 @@ class LightService:
         if light_type == LightType.LIVING_ROOM_FLOOR_LAMP:
             try:
                 status = LightService.floor_lamp.toggle()
-            except Exception as e:
+            except BulbException as e:
                 print (e)
                 status = "error"
         return status
@@ -60,18 +71,29 @@ class LightService:
     @staticmethod
     def adjust_brightness(light_type: object, amount: int):
         status = ""
-        if light_type == LightType.LIVING_ROOM_FLOOR_LAMP:
-            try:
-                status = LightService.floor_lamp.set_brightness(amount)
-            except Exception as e:
-                print(e)
-                status = "error"
-            return status
+        lamp_properties = LightService.floor_lamp.get_properties()
+        is_already_off = lamp_properties['power'] == 'off'
+        if light_type == LightType.LIVING_ROOM_FLOOR_LAMP and not is_already_off:
+            for attempt in range(3):
+                try:
+                    status = LightService.floor_lamp.set_brightness(amount)
+                except BulbException as e:
+                    print (f'Attempt {attempt}: {e}')
+                    status = "error"
+                except Exception as e:
+                    print (f'Attempt {attempt}: {e}')
+                    status = "error"
+                else:
+                    break # exit attempt loop
+        return status
 
 
 if __name__ == "__main__":
-    print(LightService.lamp_properties)
-    # LightService.turn_off(LightType.LIVING_ROOM_FLOOR_LAMP)
+    LightService()
+    # print(LightService.lamp_properties)
+    LightService.turn_on(LightType.LIVING_ROOM_FLOOR_LAMP)
+    sleep(5)
+    LightService.turn_off(LightType.LIVING_ROOM_FLOOR_LAMP)
 
          
 
